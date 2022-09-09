a1202e2f84d98e77237e24fcdf12d631bbcdd2ba
DESKTOP-M5UF1IQ\orlov <orlovaad@gmail.com>
2022-09-09 15:32:54 +0400

    create component menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import background from "./img/Background.png";
 import './App.css';
 import Menu from './components/menu/Menu';
 
 function App() {
+  const handleSetLevel = (level: string) => {
+    console.log(level);
+  }
   return (
-    <div className="App">
-      <Menu />
+    <div style={{ backgroundImage: `url(${background})`,  height: '100vh', backgroundRepeat: 'no-repeat'}} >
+      <div className="App" >
+        <Menu setLevel={handleSetLevel}/>
+      </div>
     </div>
   );
 }
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,10 +1,42 @@
-import './Menu.css';
+import './Menu.css'
+import { useState } from 'react'
 
-function Menu() {
+interface MenuProps {
+    setLevel: (level: string) => void;
+}
+
+function Menu(props: MenuProps) {
+    const [state, setState] = useState({ value: '' });
+    const setLevel = props.setLevel;
+    const setEasy = () => {
+        setState({ value: 'easy' });
+    }
+    const setMedium = () => {
+        setState({ value: 'medium' });
+    }
+    const setHard = () => {
+        setState({ value: 'hard' });
+    }
+
+    const handleButton = () => {
+        setLevel(state.value);
+        setState({ value: '' });
+    }
 
     return (
-        <div>
-            <header>HeaderTest</header>
+        <div className='menu'>
+            <div>
+                <header className='header'>Выберите уровень сложности:</header>
+                <p className='headerSecond'>Выберите сложность и игра начнется. Чем выше уровень - тем больше будет выборка карт. </p>
+            </div>
+            <div className='buttonsLevel'>
+                <ul>
+                <li><button className={`buttonLevel ${state.value === 'easy' ? 'active' : ''}`} onClick={setEasy}>Простой</button></li>
+                <li><button className={`buttonLevel ${state.value === 'medium' ? 'active' : ''}`} onClick={setMedium}>Средний</button></li>
+                <li><button className={`buttonLevel ${state.value === 'hard' ? 'active' : ''}`} onClick={setHard}>Сложный</button></li>
+                </ul>
+            </div>
+            <button className='startButton' onClick={handleButton}>Начать игру</button>
         </div>
     );
 }
